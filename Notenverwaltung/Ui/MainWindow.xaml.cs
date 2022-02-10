7f386992310ac4a48cf2cfc07443d4132b5ee9e4
zipfelp <zipfelp@NUCD20803>
2022-02-10 11:57:31 +0100

    Projektdateien hinzufügen.

---
+++ Notenverwaltung/Ui/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Notenverwaltung
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            lblSchliessen.MouseLeftButtonDown += (sender, e) => this.Close();
+            lblMinimieren.MouseLeftButtonDown += (sender, e) => this.WindowState = WindowState.Minimized;
+        }
+    }
+}
